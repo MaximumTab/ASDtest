5f43a7aeed041516881e1fd806cbd989b494964a
MaximumTab <maximumtab@outlook.com>
2025-06-10 16:55:35 +1000

    1

---
+++ ASDTEST.csproj/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASDTEST.csproj.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
